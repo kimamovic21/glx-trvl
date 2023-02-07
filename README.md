9d362dd178114bf503404f093ba427b3b626a4c6
unknown <imamovic.kerim@gmail.com>
2023-02-07 14:31:26 +0100

    izmjene
    -izmjene u fajlovima

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Getting Started with Create React App
 
+![weekaway-reactjs-tailwindcss-cc-2022](glx-trvl.jpg)
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 import Contact from './routes/Contact';
-import Home from './routes/Home'
+import Home from './routes/Home';
 import Pricing from './routes/Pricing';
 import Training from './routes/Training';
 
 function App() {
   return (
     <>
-      <Routes>
+     <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/pricing' element={<Pricing/>}/>
         <Route path='/training' element={<Training/>}/>
         <Route path='/contact' element={<Contact/>}/>
-      </Routes>
+     </Routes>
     </>
   );
 }
